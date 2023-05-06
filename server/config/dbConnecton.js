0f875cb83b6999fb3cf9665ff4f6e6ca66e2f2f6
Chetan baliyan <chetanbaliyan10@gmail.com>
2023-05-06 23:50:57 +0530

    change db

--- server/config/dbConnecton.js
+++ server/config/dbConnecton.js
@@ -24,15 +24,11 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY ?? "";
 
 const db = new Polybase({
   baseURL: `https://testnet.polybase.xyz/v0`,
-  defaultNamespace:
-    "pk/0x30dd37d78888d4ba3c26ac0ba5dc09eb01869e547444df1b419618521a4ebe10668d339496e76d30344e8461a2037af1abb13154baa5ec4bf2db28cbaf2103c9/test",
+  defaultNamespace: process.env.NAMESPACE,
 
   signer: async (data) => {
     const privateKey = Buffer.from(
-      decodeFromString(
-        "5d82fcb21470b5afa3bef64d6502e6182fb23d1131bd489db57982858c6e1f48",
-        "hex"
-      )
+      decodeFromString(process.env.PRIVATE_KEY, "hex")
     );
     return { h: "eth-personal-sign", sig: ethPersonalSign(privateKey, data) };
   },
