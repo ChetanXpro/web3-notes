7b82e1eedf05c997d73eb43165c742547aecde82
Chetan baliyan <chetanbaliyan10@gmail.com>
2023-05-07 13:37:45 +0530

    fix cors

--- server/config/corsOptons.js
+++ server/config/corsOptons.js
@@ -1,15 +1,6 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://du-web-five.vercel.app",
-];
-
 const corsOption = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not alowed by CORS"));
-    }
+    callback(null, true);
   },
   credentials: true,
   optionsSuccessStatus: 200,
